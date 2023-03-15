213e4dcca5cf2eb2cf5713266063d2154dea3b01
mimovr <xialugui@mimovr.com>
2023-03-15 11:34:26 +0800

    feature(首页): 增加博客卡片组件。

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <n-card :bordered="true"
+          style="border-width: .2rem;border-color: black;padding:.8rem 0;border-radius: .5rem;width: 40rem;"
+          content-style="display:flex;text-align: left;font-weight:bold;font-size:xx-large"
+          footer-style="display:flex;justify-content: left;"
+  >
+    <template #cover>
+      <n-image src="./src/assets/img1.png" style="height: 100%;"/>
+    </template>
+    <template #header>
+      <n-space>
+        <n-tag round :bordered="false" style="background-color: black;color: white">
+          类型
+        </n-tag>
+        <n-tag :bordered="false" :color="{color: 'white'}">
+          时间
+        </n-tag>
+      </n-space>
+    </template>
+    <n-ellipsis :line-clamp="2">
+      电灯熄灭 物换星移 泥牛入海<br>黑暗好像 一颗巨石 按在胸口<br>独脚大盗
+      百万富翁 摸爬滚打<br>黑暗好像 一颗巨石 按在胸口
+    </n-ellipsis>
+
+    <template #footer>
+      <n-space style="height: 5rem">
+        <n-avatar
+            bordered
+            style="border-style: solid;border-color: black;"
+            round
+            :size="60"
+            src="../src/assets/logo.png"
+        />
+        <div
+            style="display: flex;flex-direction: column;align-items: flex-start;justify-content: center;height: 100%;">
+          <div style="font-weight:bold;font-size:large;">陆龟</div>
+          <div style="color: #adadad;">二〇二三年二月二十一日</div>
+        </div>
+
+      </n-space>
+    </template>
+  </n-card>
+</template>
+
+<script setup lang="ts">
+import {NAvatar, NCard, NEllipsis, NImage, NSpace, NTag} from "naive-ui";</script>
+
+<style scoped>
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,6 +10,7 @@ import MouseWheel from "@better-scroll/mouse-wheel";
 import ScrollBar from "@better-scroll/scroll-bar";
 import SH2 from "../components/SH2.vue";
 import SH1 from "../components/SH1.vue";
+import BlogCard from "../components/BlogCard.vue";
 
 BScroll.use(MouseWheel)
 BScroll.use(PullUp)
@@ -47,7 +48,7 @@ function init() {
 
 onMounted(() => {
   init()
-  cardb.value.children[0].children[0].style = "width:auto;margin-left:1rem;margin-right:1rem;box-shadow:0 .5rem 1.2rem rgba(215,215,215,5);height:25rem;border-radius: 1rem;";
+  // cardb.value.children[0].children[0].style = "width:auto;margin-left:1rem;margin-right:1rem;box-shadow:0 .5rem 1.2rem rgba(215,215,215,5);height:25rem;border-radius: 1rem;";
 })
 
 </script>
@@ -76,61 +77,70 @@ onMounted(() => {
           shu
         </s-h3>
         <s-h2>
-          <s-h1 style="display: inline-flex">鼠</s-h1>牛虎兔龙蛇马羊猴鸡狗猪
+          <s-h1 style="display: inline-flex">鼠</s-h1>
+          牛虎兔龙蛇马羊猴鸡狗猪
         </s-h2>
         <s-h3 style="color:#adadad ">
           一个有点意思的博客，不妨试试！
         </s-h3>
       </n-space>
-      <div id="selected-articles" style="display:flex;margin:0 auto;width: 84rem;justify-content: space-between"
-           ref="cardb">
-        <n-card :bordered="true"
-                style="border-width: .2rem;border-color: black;padding:.8rem .0rem;border-radius: .5rem;width: 40rem;"
-                content-style="display:flex;text-align: left;font-weight:bold;font-size:xx-large"
-                footer-style="display:flex;justify-content: left;"
-        >
-          <template #cover>
-            <n-image src="./src/assets/img1.png" style="height: 100%;"/>
-          </template>
-          <template #header>
-            <n-space>
-              <n-tag round :bordered="false" style="background-color: black;color: white">
-                类型
-              </n-tag>
-              <n-tag :bordered="false" :color="{color: 'white'}">
-                时间
-              </n-tag>
-            </n-space>
-          </template>
-          <n-ellipsis :line-clamp="2">
-            电灯熄灭 物换星移 泥牛入海<br>黑暗好像 一颗巨石 按在胸口<br>独脚大盗
-            百万富翁 摸爬滚打<br>黑暗好像 一颗巨石 按在胸口
-          </n-ellipsis>
+      <n-space>
+        <blog-card/>
+        <n-space vertical justify="space-between" align="center">
+          <article-introduce/>
+          <article-introduce/>
+          <article-introduce/>
+        </n-space>
+      </n-space>
+      <!--      <div id="selected-articles" style="display:flex;margin:0 auto;width: 84rem;justify-content: space-between"
+                 ref="cardb">
+              <n-card :bordered="true"
+                      style="border-width: .2rem;border-color: black;padding:.8rem .0rem;border-radius: .5rem;width: 40rem;"
+                      content-style="display:flex;text-align: left;font-weight:bold;font-size:xx-large"
+                      footer-style="display:flex;justify-content: left;"
+              >
+                <template #cover>
+                  <n-image src="./src/assets/img1.png" style="height: 100%;"/>
+                </template>
+                <template #header>
+                  <n-space>
+                    <n-tag round :bordered="false" style="background-color: black;color: white">
+                      类型
+                    </n-tag>
+                    <n-tag :bordered="false" :color="{color: 'white'}">
+                      时间
+                    </n-tag>
+                  </n-space>
+                </template>
+                <n-ellipsis :line-clamp="2">
+                  电灯熄灭 物换星移 泥牛入海<br>黑暗好像 一颗巨石 按在胸口<br>独脚大盗
+                  百万富翁 摸爬滚打<br>黑暗好像 一颗巨石 按在胸口
+                </n-ellipsis>
 
-          <template #footer>
-            <n-space style="height: 5rem">
-              <n-avatar
-                  bordered
-                  style="border-style: solid;border-color: black;"
-                  round
-                  :size="60"
-                  src="../src/assets/logo.png"
-              />
-              <div
-                  style="display: flex;flex-direction: column;align-items: flex-start;justify-content: center;height: 100%;">
-                <div style="font-weight:bold;font-size:large;">陆龟</div>
-                <div style="color: #adadad;">二〇二三年二月二十一日</div>
+                <template #footer>
+                  <n-space style="height: 5rem">
+                    <n-avatar
+                        bordered
+                        style="border-style: solid;border-color: black;"
+                        round
+                        :size="60"
+                        src="../src/assets/logo.png"
+                    />
+                    <div
+                        style="display: flex;flex-direction: column;align-items: flex-start;justify-content: center;height: 100%;">
+                      <div style="font-weight:bold;font-size:large;">陆龟</div>
+                      <div style="color: #adadad;">二〇二三年二月二十一日</div>
+                    </div>
+
+                  </n-space>
+                </template>
+              </n-card>
+              <div style="display: flex;flex-direction:column;justify-content: space-around">
+                <article-introduce/>
+                <article-introduce/>
+                <article-introduce/>
               </div>
-
-            </n-space>
-          </template>
-        </n-card>
-        <div style="display: flex;flex-direction:column;justify-content: space-around">
-          <article-introduce/>
-          <article-introduce/>
-          <article-introduce/>
-        </div>
-      </div>
+            </div>-->
 
     </n-space>
     <n-space vertical size="large">
@@ -175,9 +185,11 @@ onMounted(() => {
   <n-back-top :right="100"/>
 </template>
 
-<style scoped>
-.n-card-cover {
-  box-shadow: 0 .2rem .2rem black;
+<style scoped lang="less">
+:deep(.n-card-cover) {
+  box-shadow: 0 1rem 10rem rgba(0, 0, 0, .15);
+  height: 25rem;
+  border-radius: 1rem;
 }
 
 #subscribe {
@@ -193,6 +205,5 @@ onMounted(() => {
 
 #introduce {
   background-image: url("../assets/mouse.png");
-  /*height: 90rem;*/
 }
 </style>
