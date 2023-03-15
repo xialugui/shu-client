c8c50537d8e2fc5cdaf796b63841ab5c93c49eed
mimovr <xialugui@mimovr.com>
2023-03-15 14:20:17 +0800

    refactor(博客小卡片): 重构博客小卡片。

--- src/components/BlogCardFooter.vue
+++ src/components/BlogCardFooter.vue
@@ -4,11 +4,13 @@
         bordered
         style="border-style: solid;border-color: black;"
         round
-        :size="60"
+        :size="size==='large'?60:47"
         src="../src/assets/logo.png"
     />
     <n-space vertical justify="center" style="height: 100%;">
-      <s-h3>陆龟</s-h3>
+
+      <s-h3 v-if="size==='large'">陆龟</s-h3>
+      <s-h5 v-else>陆龟</s-h5>
       <s-h5 style="color:#adadad ">二〇二三年二月二十一日</s-h5>
     </n-space>
   </n-space>
@@ -18,8 +20,12 @@
 
 import {NAvatar, NSpace} from "naive-ui";
 import SH5 from "./SH5.vue";
-import SH3 from "./SH3.vue";</script>
+import SH3 from "./SH3.vue";
 
-<style scoped>
+withDefaults(defineProps<{ size?: 'small' | 'large' }>(), {
+  size: 'large'
+});
+</script>
 
+<style scoped lang="less">
 </style>
